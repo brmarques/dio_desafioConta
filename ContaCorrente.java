f709c1ceb1213c4abd7ac86133097408e4aa255c
Braulio <braulio007@gmail.com>
2023-08-15 19:04:40 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -5,6 +5,26 @@ public class ContaCorrente extends Conta {
     }
 
     public void calculaValorTarifaManutencao() {
-        // Lógica específica para tarifa de manutenção de conta corrente
+        // Lógica para tarifa de manutenção de investimento
+        if (contaInativaAteSeisMeses()) {
+            double tarifa = getSaldo() * 0.02; // 2% de tarifa
+            System.out.println("Tarifa de manutenção aplicada: " + tarifa);
+            sacar(tarifa);
+        }
+    }
+
+    private boolean contaInativaAteSeisMeses() {
+        // Obtém a data atual (estou considerando que você tem uma classe que fornece a data atual)
+        String dataAtual = DataUtils.getDataAtual(); // Substitua DataUtils.getDataAtual() pelo método que você utiliza para obter a data atual
+
+        // Calcula a diferença entre a última atividade e a data atual em meses
+        int mesesInativos = DataUtils.calcularMesesEntreDatas(ultimaAtividade, dataAtual); // Substitua DataUtils.calcularMesesEntreDatas() pelo método que calcula a diferença em meses entre duas datas
+
+        // Retorna true se a conta estiver inativa por até seis meses
+        return mesesInativos <= 6;
     }
 }
+
+
+
+
